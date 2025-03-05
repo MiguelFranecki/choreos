fc7248c58e026f5231cc9daddbe2d849297b0671
MiguelFranecki <120767385+MiguelFranecki@users.noreply.github.com>
2025-03-05 16:38:20 +1200

    Update index.js

--- files/index.js
+++ files/index.js
@@ -6,8 +6,8 @@ const UUID = process.env.UUID || '80a991f6-4076-4c1d-be1e-0b8573aa5bd6'; //若�
 const NEZHA_SERVER = process.env.NEZHA_SERVER || 'nz.f4i.cn';     
 const NEZHA_PORT = process.env.NEZHA_PORT || '5555';                     // 哪吒端口为{443,8443,2096,2087,2083,2053}其中之一开启tls
 const NEZHA_KEY = process.env.NEZHA_KEY || '5ddVS93Eq0Uc9he880';
-const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'choreo.balls.dedyn.io';     // 建议使用token，argo端口8080，cf后台设置需对应,使用json需上传json和yml文件至files目录
-const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiOGE0YTU0NjIxMDRjZTA1YzNmZDg4YmIxMTBmMTM3NWIiLCJ0IjoiZTcxZjlmYzItZjVlMy00MDI1LTg2YmYtNDBlZjBjOTRiY2ZiIiwicyI6Ik9HUXpPVGMwTWprdE5qTTNNQzAwWldFeExUZzRaRE10WlRrMk5EZzRaakJpWVdKbCJ9';
+const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'choreo.cctalk.us.kg';     // 建议使用token，argo端口8080，cf后台设置需对应,使用json需上传json和yml文件至files目录
+const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiOGU1MGRkNjJiMjczOTE5YWM4NTgzYTMzZDNkMjljODMiLCJ0IjoiYTM3ODk3MzItNzgwZi00YzQ1LWI1MjYtNWRhMzYzZjU5ODdhIiwicyI6Ik5XVmlOREUyTldFdFlqa3pPUzAwWVdVd0xXSXlOekV0WmpNM1l6RTVNbUl4T1RReSJ9';
 const CFIP = process.env.CFIP || 'na.ma';
 const NAME = process.env.NAME || 'Choreo';
 
